0e0e645246796dcb5e2914e51efd7addf8639f4a
Daniel Schnurpfeil <117296223+danschnurp@users.noreply.github.com>
2023-02-24 17:12:32 +0100

    update project structure

--- 02/bonus.py
+++ 02/bonus.py
@@ -5,6 +5,12 @@ from config import path_to_images
 
 
 def create_representation(img):
+    """
+    It takes an image and returns the Hu moments of the image
+
+    :param img: The image to be processed
+    :return: HuMoments
+    """
     return cv2.HuMoments(cv2.moments(img))
 
 
@@ -13,8 +19,10 @@ if __name__ == '__main__':
     representations = np.zeros((15, 7), dtype=np.float32)
 
     for i in range(1, 16):
+        # Creating a representation of the image.
         representations[i - 1] = np.squeeze(create_representation(
             cv2.bitwise_not(cv2.imread(
+                # Reading the image from the path and converting it to grayscale.
                 path_to_images() + str(i) + ".png", cv2.IMREAD_GRAYSCALE))))
 
     # print(representations)
@@ -26,6 +34,7 @@ if __name__ == '__main__':
     flags = cv2.KMEANS_RANDOM_CENTERS
     compactness, labels, centers = cv2.kmeans(representations, 3, None, criteria, 10, flags)
 
+    # A dictionary that maps the true labels of the images to the index of the image.
     true_labels = {"A": [0, 2, 3, 4, 9, 10], "X": [1, 5, 7, 11, 14], "K": [6, 8, 12, 13]}
     labels_dict = {}
     for index, i in enumerate(np.squeeze(labels)):
--- config.py
+++ config.py
@@ -1,10 +1,13 @@
-#  date: 21. 2. 2023
-#  author: Daniel Schnurpfeil
-#
 import os
 
 
 def path_to_images(output_filename="images", output_dir="../") -> str:
+    """
+    > This function returns the path to the images directory
+
+    :param output_filename: The name of the folder that will be created to store the images, defaults to images (optional)
+    :param output_dir: The directory to save the images to, defaults to ../ (optional)
+    """
     if output_filename not in os.listdir(output_dir):
         if output_dir[-1] != "/":
             output_dir += "/"
