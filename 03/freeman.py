813427d83c9c8ac18a4bc3ebd55a6d6405cb127f
Daniel Schnurpfeil <117296223+danschnurp@users.noreply.github.com>
2023-03-04 15:29:34 +0100

    doc commentaries

--- 03/freeman.py
+++ 03/freeman.py
@@ -1,3 +1,7 @@
+#  date: 04. 03. 2023
+#  author: Daniel Schnurpfeil
+#
+
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -6,15 +10,37 @@ from utils import path_to_images, show_image_now
 
 
 def shift1(arr):
+    """
+    It shifts the elements of the array to the right by one.
+    
+    :param arr: the array to be shifted
+    """
     return np.roll(arr, -1)
 
 
 def print_area(cont):
+    """
+    This function takes a list of contours and prints the area of each contour.
+    
+    :param cont: the contour you want to find the area of
+    """
     print("Contour detection (inner border) area")
     print(0.5 * np.sum((shift1(cont[:, 0]) - cont[:, 0]) * (cont[:, 1] + shift1(cont[:, 1])).T))
 
 
 def get_4_neighbourhood(first_x: int, second_x: int, first_y: int, second_y: int) -> int:
+    """
+    > This function returns the number of neighbours of a cell in a 2D grid
+    
+    :param first_x: the x coordinate of the first point
+    :type first_x: int
+    :param second_x: the x coordinate of the second point
+    :type second_x: int
+    :param first_y: the y coordinate of the first point
+    :type first_y: int
+    :param second_y: the y coordinate of the second point
+    :type second_y: int
+    """
     if first_x < second_x:
         return 0
 
@@ -29,6 +55,13 @@ def get_4_neighbourhood(first_x: int, second_x: int, first_y: int, second_y: int
 
 
 def compute_freeman_chaincode_4_neighbourhood(contours: np.ndarray):
+    """
+    It computes the Freeman chain code for each contour in the input array
+    
+    :param contours: a list of contours, each contour is a list of points, each point is a list of x,y
+    coordinates
+    :type contours: np.ndarray
+    """
     shifted = np.roll(contours, 1, axis=0)
     return np.array([get_4_neighbourhood(x1, x2, y1, y2)
                      for x1, x2, y1, y2 in zip(contours[:, 0], shifted[:, 0], contours[:, 1], shifted[:, 1])])
