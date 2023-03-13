5e9745c52c33b04acddbd33b89b30edbf578b6d9
Daniel Schnurpfeil <117296223+danschnurp@users.noreply.github.com>
2023-03-13 17:20:03 +0100

    sp experiments jupyter version

--- SP/experiments.py
+++ SP/experiments.py
@@ -1,23 +1,20 @@
+# %%
+
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-import argparse
-
-parser = argparse.ArgumentParser(description='neco')
-parser.add_argument('-i', '--picture',
-                    help='no...',
-                    )
-
-args = parser.parse_args()
+# %%
 
 # A command line argument.
-input_name = args.picture
+input_name = "UAZK-B2-a-04-C-1425-006.JPG"
 
 # It reads the image from the file and converts it to grayscale.
 img = (cv2.imread("./data_katastr/" + input_name, cv2.IMREAD_GRAYSCALE))
 
 assert img is not None, "file could not be read, check with os.path.exists()"
+
+# %%
 
 # A Canny edge detector.
 edges = cv2.Canny(img, 1, 500)
@@ -29,14 +26,20 @@ lsd = cv2.createLineSegmentDetector(0)
 lines = lsd.detect(img)[0]  # Position 0 of the returned tuple are the detected lines
 print(lines.shape)
 
+# %%
+
 # It creates a white image with the same size as the input image.
 white = np.zeros_like(img)
 white[:] = 255
+
+# %%
 
 # Draw detected lines in the image
 drawn_img = lsd.drawSegments(white, lines)
 # It converts the image from BGR to grayscale.
 drawn_img = cv2.cvtColor(drawn_img, cv2.COLOR_BGR2GRAY)
+
+# %%
 
 # Taking the right 90% of the image.
 right_border = int(drawn_img.shape[1] * 0.9)
@@ -47,20 +50,31 @@ bottom_border = int(drawn_img.shape[0] * 0.9)
 # Taking the top 10% of the image.
 top_border = int(drawn_img.shape[0] * 0.1)
 
+# %%
+
 # Taking the right, left, bottom and top border of the image.
 right_border = drawn_img[:, right_border:]
 left_border = drawn_img[:, :left_border]
 bottom_border = drawn_img[bottom_border:, :]
 top_border = drawn_img[:top_border, :]
 
+# %%
 
 # Showing the image.
 plt.imshow(right_border, cmap='gray')
 plt.show()
+
+# %%
+
 plt.imshow(left_border, cmap='gray')
 plt.show()
+
+# %%
+
 plt.imshow(bottom_border, cmap='gray')
 plt.show()
+
+# %%
+
 plt.imshow(top_border, cmap='gray')
 plt.show()
-
