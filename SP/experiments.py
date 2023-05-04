80318cef9e9937a0354fc772d0d7c55e99e28649
danschnurpfeil <dschnurp@email.cz>
2023-05-04 15:14:50 +0200

    sp

--- SP/experiments.py
+++ SP/experiments.py
@@ -1,56 +1,20 @@
-# %%
-
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-from skimage.filters import threshold_sauvola
-from skimage import img_as_ubyte
-from skimage.transform import hough_line, hough_line_peaks, probabilistic_hough_line
+from skimage.transform import probabilistic_hough_line
 
-import os
-
-# A command line argument.
-input_dir = "/Users/danschnurpfeil/PycharmProjects/kiv-zvi/SP/data_katastr/"
-
-img_names = os.listdir(input_dir)
-for i in img_names:
-    print(i)
-print(len(img_names), "pictures loaded")
-input_name = input_dir + img_names[9]
-
-print(input_name)
-
-# %%
-"""
-##### LOADING BW
-"""
+def load_BW(input_picture_path):
+    return cv2.imread(input_picture_path, cv2.IMREAD_GRAYSCALE)
 
 
-# %%
-def load_BW():
-    return cv2.imread(input_name, cv2.IMREAD_GRAYSCALE)
 
-
-# %%
-"""
-#### LOADING COLORED
-"""
-
-
-# %%
 # It reads the image from the file.
-def load_colored():
+def load_colored(input_picture_path):
     # img = img[:,:,0]
-    return cv2.cvtColor((cv2.imread(input_name, cv2.COLOR_BGR2RGB)), cv2.COLOR_BGR2RGB)
+    return cv2.cvtColor((cv2.imread(input_picture_path, cv2.COLOR_BGR2RGB)), cv2.COLOR_BGR2RGB)
 
 
-# %%
-"""
-- convolving image by horizontal line detection mask
-"""
 
-
-# %%
 def convolve_horizontal_lightly(img):
     kernel_horizontal_lines = np.array([[-1, -1, -1],
                                         [2, 2, 2],
@@ -59,13 +23,8 @@ def convolve_horizontal_lightly(img):
     return cv2.filter2D(img, -1, kernel_horizontal_lines)
 
 
-# %%
-"""
-- convolving image by vertical line detection mask
-"""
 
 
-# %%
 def convolve_vertical_lightly(img):
     kernel_horizontal_lines = np.array([[-1, 2, -1],
                                         [-1, 2, -1],
@@ -74,45 +33,7 @@ def convolve_vertical_lightly(img):
     return cv2.filter2D(img, -1, kernel_horizontal_lines)
 
 
-# %%
-"""
-- A Canny edge detector 
-"""
-
-# %%
-# img = cv2.Canny(img, 1, 500)
-# ...
-# kernel = np.ones((3,3), np.uint8)
-# kernel[:,:] = 5
-# print(kernel)
-# img = cv2.dilate(img, kernel)
-# img = cv2.erode(img, kernel)
-# img = cv2.bitwise_not(img)
-
-# %%
-img = load_BW()
-
-assert img is not None, "file could not be read, check with os.path.exists()"
-
-# plt.imshow(img, cmap="gray")
-# plt.show()
-
-ret, thresh1 = cv2.threshold(img, 100, 155, cv2.THRESH_OTSU)
-
-# plt.imshow(thresh1, cmap="gray")
-img = thresh1
-
-
-# %%
-# sauvola = (cv2.imread(input_name, cv2.IMREAD_GRAYSCALE))
-# assert img is not None, "file could not be read, check with os.path.exists()"
-# sauvola = threshold_sauvola(img, window_size=5, k=0.1)
-
-# plt.imshow(sauvola, cmap="gray")
-# img = sauvola
-
-# %%
-def get_sizes_procentual(procentual: float):
+def get_sizes_procentual(procentual: float, img):
     # Taking the right xx% of the image.
     right_border = int(img.shape[1] * procentual)
     # Taking the left xx% of the image.
@@ -124,54 +45,29 @@ def get_sizes_procentual(procentual: float):
     return right_border, left_border, bottom_border, top_border
 
 
-# %%
-"""
-### cropping image by 1% from each side
-"""
-
-
-# %%
 def crop_one_percent(img_to_crop):
-    right_border_to_crop, left_border_to_crop, bottom_border_to_crop, top_border_to_crop = get_sizes_procentual(0.01)
+    right_border_to_crop, left_border_to_crop, bottom_border_to_crop, top_border_to_crop = get_sizes_procentual(0.01,
+                                                                                                                img_to_crop)
     print(bottom_border_to_crop, top_border_to_crop, right_border_to_crop, left_border_to_crop)
     print(img_to_crop.shape)
     return img_to_crop[bottom_border_to_crop:top_border_to_crop, right_border_to_crop:left_border_to_crop]
 
 
-img = crop_one_percent(img)
-
-# plt.imshow(img, cmap="gray")
-
-
-# %%
-# todo danger with spaghetti pictures works badly
-# Taking the right 90% of the image.
-right_border = int(img.shape[1] * 0.9)
-# Taking the left 10% of the image.
-left_border = int(img.shape[1] * 0.1)
-# Taking the bottom 90% of the image.
-bottom_border = int(img.shape[0] * 0.9)
-# Taking the top 10% of the image.
-top_border = int(img.shape[0] * 0.1)
-
-# Taking the right, left, bottom and top border of the image.
-right_border = img[:, right_border:]
-left_border = img[:, :left_border]
-bottom_border = img[bottom_border:, :]
-top_border = img[:top_border, :]
-print(top_border.shape)
+def get_borders_x_percent(img_input, x=10):
+    # todo danger with spaghetti pictures works badly
+    x /= 100
+    # Taking the right x% of the image.
+    # Taking the left x% of the image.
+    # Taking the bottom x% of the image.
+    # Taking the top x% of the image.
+    return int(img_input.shape[1] * (1 - x)), \
+           int(img_input.shape[1] * x), \
+           int(img_input.shape[0] * (1 - x)), \
+           int(img_input.shape[0] * x)
 
 
-# top_border = top_border.astype(int)
-# top_border = img_as_ubyte(top_border)
-# print(top_border[:5])
-
-# %%
 def euclidean_2D(x1, x2, y1, y2):
     return np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-
-
-# %%
 
 
 def get_vertical(x1, x2, y1, y2, theta=2, min_value=800):
@@ -190,8 +86,6 @@ def get_vertical(x1, x2, y1, y2, theta=2, min_value=800):
 # def connect_lines(x1, x2, y1, y2, theta=2, min_value=200):
 #     pass
 
-
-# %%
 
 def LDS(border):
     # Create default parametrization LSD
@@ -218,24 +112,25 @@ def hough(border):
     return lines
 
 
-def plot_border_line(longest_lines, border, original_img):
-
-    # todo compose img back with red highlights
-
+def plot_border_line(longest_lines, border):
     # Draw detected lines in the image
     for line_to_draw in longest_lines:
-        border = cv2.line(border, (line_to_draw[0], line_to_draw[1]), (line_to_draw[2], line_to_draw[3]), (0))
+        border = cv2.line(border, (line_to_draw[0], line_to_draw[1]), (line_to_draw[2], line_to_draw[3]), (255, 0, 0),
+                          thickness=100)
     print(border.shape)
-    # It converts the image from BGR to grayscale.
-    # drawn_img = cv2.cvtColor(white, cv2.COLOR_BGR2GRAY)
+    # kernel = np.zeros((3, 3))
+    # kernel[1] = -1
+    # kernel.T[1] = -1
+    # kernel[1, 1] = 5
+    # # plt.imshow(top_border, cmap="gray")
+    # border = cv2.filter2D(border, -1, kernel)
     # plt.imshow(border, cmap="gray")
+    # plt.show()
+
     return border
 
 
-# %%
-
-
-def make_line_detection(border, detection_method, img_shape, original_img, horizontal_only=True, vertical_only=False):
+def make_line_detection(border, detection_method, img_shape, horizontal_only=True, vertical_only=False):
     if horizontal_only:
         for _ in range(9):
             border = convolve_horizontal_lightly(border)
@@ -278,27 +173,73 @@ def make_line_detection(border, detection_method, img_shape, original_img, horiz
     longest_lines = longest_lines.astype(int)
     #     print(longest_lines)
 
-    border[:, :] = 255
-
-    return plot_border_line(longest_lines=longest_lines, border=border, original_img=original_img)
+    return longest_lines
 
 
-# %%
+def main(image_name, input_picture_path):
+    img = load_BW(input_picture_path)
 
-original_img = load_colored()
-original_img = crop_one_percent(original_img)
+    assert img is not None, "file could not be read, check with os.path.exists()"
 
-# %%
-make_line_detection(right_border, hough, horizontal_only=False, vertical_only=True,
-                    img_shape=img.shape, original_img=original_img)
+    # plt.imshow(img, cmap="gray")
+    # plt.show()
 
-# %%
-make_line_detection(left_border, hough, horizontal_only=False, vertical_only=True,
-                    img_shape=img.shape, original_img=original_img)
+    ret, thresh1 = cv2.threshold(img, 100, 155, cv2.THRESH_OTSU)
 
-# %%
-make_line_detection(top_border, hough, horizontal_only=True, vertical_only=False, img_shape=img.shape,
-                    original_img=original_img)
+    # plt.imshow(thresh1, cmap="gray")
+    img = thresh1
 
-# %%
-make_line_detection(bottom_border, hough, horizontal_only=True, img_shape=img.shape, original_img=original_img)
+    # sauvola = (cv2.imread(input_name, cv2.IMREAD_GRAYSCALE))
+    # assert img is not None, "file could not be read, check with os.path.exists()"
+    # sauvola = threshold_sauvola(img, window_size=5, k=0.1)
+
+    # plt.imshow(sauvola, cmap="gray")
+    # img = sauvola
+
+    img = crop_one_percent(img)
+
+    # plt.imshow(img, cmap="gray")
+
+    right_border, left_border, bottom_border, top_border = get_borders_x_percent(img)
+
+    # Taking the right, left, bottom and top border of the image.
+    right_border = img[:, right_border:]
+    left_border = img[:, :left_border]
+    bottom_border = img[bottom_border:, :]
+    top_border = img[:top_border, :]
+    print(top_border.shape)
+
+    lines = [
+        make_line_detection(top_border, hough, horizontal_only=True, vertical_only=False, img_shape=img.shape),
+        make_line_detection(bottom_border, hough, horizontal_only=True, img_shape=img.shape),
+        make_line_detection(left_border, hough, horizontal_only=False, vertical_only=True,
+                            img_shape=img.shape),
+        make_line_detection(right_border, hough, horizontal_only=False, vertical_only=True,
+                            img_shape=img.shape)
+
+    ]
+
+    img = load_colored(input_picture_path)
+    img = crop_one_percent(img)
+
+    right_border, left_border, bottom_border, top_border = get_borders_x_percent(img)
+
+    parts = [img[:top_border, :],
+             img[bottom_border:, :],
+             img[:, :left_border],
+             img[:, right_border:], ]
+
+    for part, line in zip(parts, lines):
+        part = plot_border_line(line, part)
+    plt.imshow(img, cmap="gray")
+    plt.savefig("./annotated_katastr/" + image_name)
+
+
+import os
+
+input_dir = "/Users/danschnurpfeil/PycharmProjects/kiv-zvi/SP/data_katastr/"
+
+img_names = os.listdir(input_dir)
+for image_name in img_names:
+    input_picture_path = input_dir + image_name
+    main(image_name=image_name, input_picture_path=input_picture_path)
